14a519ceec627e2001455906a920d7e02f266880
Minh Le <59521026+minhcrafters@users.noreply.github.com>
2023-10-16 13:38:20 +0000

    a lot ig

--- commands/pause.js
+++ commands/pause.js
@@ -1,25 +1,25 @@
 const db = require("../mongoDB");
 module.exports = {
-  name: "pause",
-  description: "Stops playing the currently playing music.",
-  permissions: "0x0000000000000800",
-  options: [],
-  voiceChannel: true,
-  run: async (client, interaction) => {
-    const queue = client.player.getQueue(interaction.guild.id);
-    let lang = await db?.musicbot?.findOne({ guildID: interaction.guild.id })
-    lang = lang?.language || client.language
-    lang = require(`../languages/${lang}.js`);
+    name: "pause",
+    description: "Stops playing the currently playing music.",
+    permissions: "0x0000000000000800",
+    options: [],
+    voiceChannel: true,
+    run: async (client, interaction) => {
+        const queue = client.player.getQueue(interaction.guild.id);
+        let lang = await db?.musicbot?.findOne({ guildID: interaction.guild.id })
+        lang = lang?.language || client.language
+        lang = require(`../languages/${lang}.js`);
 
-    try {
+        try {
 
-      if (!queue || !queue.playing) return interaction.reply({ content: lang.msg5, ephemeral: true }).catch(e => { })
-      const success = queue.pause();
-      return interaction.reply({ content: success ? `**${queue.songs[0].name}** - ${lang.msg48}` : lang.msg41 }).catch(e => { })
+            if (!queue || !queue.playing) return interaction.reply({ content: lang.msg5, ephemeral: true }).catch(e => { })
+            const success = queue.pause();
+            return interaction.reply({ content: success ? `**${queue.songs[0].name}** - ${lang.msg48}` : lang.msg41 }).catch(e => { })
 
-    } catch (e) {
-      const errorNotifer = require("../functions.js")
-     errorNotifer(client, interaction, e, lang)
-      }
-  },
+        } catch (e) {
+            const errorNotifer = require("../functions.js")
+            errorNotifer(client, interaction, e, lang)
+        }
+    },
 }
--- commands/servers.js
+++ commands/servers.js
@@ -12,75 +12,75 @@ module.exports = {
     permissions: "0x0000000000000800",
     run: async (client, interaction) => {
 
-        if(!client.config.ownerID.includes(interaction?.user?.id)) return interaction.reply({ content: "You don't have permission to use this command.", ephemeral: true }).catch(e => { });
+        if (!client.config.ownerID.includes(interaction?.user?.id)) return interaction.reply({ content: "You don't have permission to use this command.", ephemeral: true }).catch(e => { });
 
 
         const server = interaction.options.getString('server');
         if (server) {
             let guild
-if(config.shardManager.shardStatus == true){
-            const getServer = async (guildID) => {
-                const req = await client.shard.broadcastEval((c, id) => c.guilds.cache.get(id), { 
-                    context: guildID
-                });
-                return req.find(res => !!res) || null;
+            if (config.shardManager.shardStatus == true) {
+                const getServer = async (guildID) => {
+                    const req = await client.shard.broadcastEval((c, id) => c.guilds.cache.get(id), {
+                        context: guildID
+                    });
+                    return req.find(res => !!res) || null;
+                }
+                guild = await getServer(server);
+                if (!guild) return interaction.reply({ content: "I'm not in that server.", ephemeral: true }).catch(e => { });
+
+                const embed = new EmbedBuilder()
+                    .setTitle(`Server Info: ${guild.name}`)
+                    .setDescription(`> **ID:** \`${guild.id}\`\n> **Members:** \`${guild.memberCount}\`\n> **Channels:** \`${guild.channels.length}\`\n> **Roles:** \`${guild.roles.length}\`\n> **Emojis:** \`${guild.emojis.length}\`\n> **Boosts:** \`${guild.premiumSubscriptionCount}\`\n> **Boost Level:** \`${guild.premiumTier}\``)
+                    .setColor(client.config.embedColor)
+                    .setThumbnail(guild.iconURL)
+                    //fetch a guild invite url
+                    .addFields([
+                        { name: "Invite", value: guild?.vanityURLCode ? `https://discord.gg/${guild?.vanityURLCode}` : "Not found invite." },
+                    ])
+                    .setTimestamp()
+                return interaction.reply({ embeds: [embed], ephemeral: true }).catch(e => { })
+            } else {
+                guild = client.guilds.cache.get(server)
+                if (!guild) return interaction.reply({ content: "I'm not in that server.", ephemeral: true }).catch(e => { });
+
+                const embed = new EmbedBuilder()
+                    .setTitle(`Server Info: ${guild.name}`)
+                    .setDescription(`> **ID:** \`${guild.id}\`\n> **Members:** \`${guild.memberCount}\`\n> **Channels:** \`${guild.channels.cache.size}\`\n> **Roles:** \`${guild.roles.cache.size}\`\n> **Emojis:** \`${guild.emojis.cache.size}\`\n> **Boosts:** \`${guild.premiumSubscriptionCount}\`\n> **Boost Level:** \`${guild.premiumTier}\``)
+                    .setColor(client.config.embedColor)
+                    .setThumbnail(guild.iconURL())
+                    //fetch a guild invite url
+                    .addFields([
+                        { name: "Invite", value: guild?.vanityURLCode ? `https://discord.gg/${guild?.vanityURLCode}` : "Not found invite." },
+                    ])
+                    .setTimestamp()
+                return interaction.reply({ embeds: [embed], ephemeral: true }).catch(e => { })
             }
-             guild = await getServer(server);
-             if (!guild) return interaction.reply({ content: "I'm not in that server.", ephemeral: true }).catch(e => { });
 
-             const embed = new EmbedBuilder()
-                 .setTitle(`Server Info: ${guild.name}`)
-                 .setDescription(`> **ID:** \`${guild.id}\`\n> **Members:** \`${guild.memberCount}\`\n> **Channels:** \`${guild.channels.length}\`\n> **Roles:** \`${guild.roles.length}\`\n> **Emojis:** \`${guild.emojis.length}\`\n> **Boosts:** \`${guild.premiumSubscriptionCount}\`\n> **Boost Level:** \`${guild.premiumTier}\``)
-                 .setColor(client.config.embedColor)
-                 .setThumbnail(guild.iconURL)
-                 //fetch a guild invite url
-                 .addFields([
-                     { name: "Invite", value: guild?.vanityURLCode ? `https://discord.gg/${guild?.vanityURLCode}` : "Not found invite." },
-                 ])
-                 .setTimestamp()
-             return interaction.reply({ embeds: [embed], ephemeral: true }).catch(e => { })
-} else {
-    guild = client.guilds.cache.get(server)
-    if (!guild) return interaction.reply({ content: "I'm not in that server.", ephemeral: true }).catch(e => { });
-
-    const embed = new EmbedBuilder()
-        .setTitle(`Server Info: ${guild.name}`)
-        .setDescription(`> **ID:** \`${guild.id}\`\n> **Members:** \`${guild.memberCount}\`\n> **Channels:** \`${guild.channels.cache.size}\`\n> **Roles:** \`${guild.roles.cache.size}\`\n> **Emojis:** \`${guild.emojis.cache.size}\`\n> **Boosts:** \`${guild.premiumSubscriptionCount}\`\n> **Boost Level:** \`${guild.premiumTier}\``)
-        .setColor(client.config.embedColor)
-        .setThumbnail(guild.iconURL())
-        //fetch a guild invite url
-        .addFields([
-            { name: "Invite", value: guild?.vanityURLCode ? `https://discord.gg/${guild?.vanityURLCode}` : "Not found invite." },
-        ])
-        .setTimestamp()
-    return interaction.reply({ embeds: [embed], ephemeral: true }).catch(e => { })
-}
-          
         } else {
             let guilds
-            if(config.shardManager.shardStatus == true){
-            //fetch all guilds
-             guilds = await client.shard.broadcastEval(c => c.guilds.cache.map(g => {
-                return {
-                    name: g.name,
-                    id: g.id,
-                    memberCount: g.memberCount
-                }
-            }));
-            //sort from largest to smallest
-            guilds = guilds.flat().sort((a, b) => b.memberCount - a.memberCount);
+            if (config.shardManager.shardStatus == true) {
+                //fetch all guilds
+                guilds = await client.shard.broadcastEval(c => c.guilds.cache.map(g => {
+                    return {
+                        name: g.name,
+                        id: g.id,
+                        memberCount: g.memberCount
+                    }
+                }));
+                //sort from largest to smallest
+                guilds = guilds.flat().sort((a, b) => b.memberCount - a.memberCount);
             } else {
                 guilds = client.guilds.cache.map(g => {
-                return {
-                    name: g.name,
-                    id: g.id,
-                    memberCount: g.memberCount
-                }
-            })
-            //sort from largest to smallest
-            guilds = guilds.flat().sort((a, b) => b.memberCount - a.memberCount);
+                    return {
+                        name: g.name,
+                        id: g.id,
+                        memberCount: g.memberCount
+                    }
+                })
+                //sort from largest to smallest
+                guilds = guilds.flat().sort((a, b) => b.memberCount - a.memberCount);
             }
-            
+
             //page system
             let page = 0;
             const maxPage = Math.ceil(guilds.length / 10) - 1;
@@ -128,4 +128,5 @@ if(config.shardManager.shardStatus == true){
             });
         }
 
-    }}
+    }
+}
